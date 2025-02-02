9816034e5581cee9f016b6076499b94b33adffad
Asem <ismailovaasem363@gmail.com>
2025-02-02 15:18:11 +0500

    ! added

--- hw5.js
+++ hw5.js
@@ -1,9 +1,9 @@
 function useLoginAndPassword (login, password) {
   if (login === 'correct' && password === 'correct') {
     console.log('you are logged in');
-  } else if (login === 'correct' && password === 'incorrect') {
+  } else if (login === 'correct' && password !== 'correct') {
     console.log('your password is wrong');
-  } else if (login === 'incorrect' && password === 'correct') {
+  } else if (login !== 'correct' && password === 'correct') {
     console.log('Your login is wrong');
   } else {
     console.log('both inputs are wrong');
